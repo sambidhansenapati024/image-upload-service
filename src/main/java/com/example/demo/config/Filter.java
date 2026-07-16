6e06e58976339174d370b7662306e84a26045c4d
Sambidhan <sambidhansenapati208@gmail.com>
2026-07-16 15:13:58 +0530

    add filter

---
+++ src/main/java/com/example/demo/config/Filter.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class Filter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("Client IP------  " + request.getRemoteAddr());
+        System.out.println("Remote User------  " + request.getRemoteUser());
+        System.out.println("Remote Host------  " + request.getRemoteHost());
+        System.out.println("Remote Port------  " + request.getRemotePort());
+        filterChain.doFilter(request,response);
+    }
+}
